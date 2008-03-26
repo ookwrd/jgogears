c80125db92f03f72e0a47a8c55b0aa571232ab31
syeates <syeates@d96caf76-6f43-0410-8f8b-1b2c4ef0ccf3>
2008-03-26 04:38:20 +0000

    ensure that all tests are actually being run

--- trunk/jgogears/jgogears/AllTests.java
+++ trunk/jgogears/jgogears/AllTests.java
@@ -18,27 +18,28 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for jgogears");
-		// $JUnit-BEGIN$
+		//$JUnit-BEGIN$
+		suite.addTestSuite(GameTest.class);
 		suite.addTestSuite(GTPParserUtilsTest.class);
 		suite.addTestSuite(GTPScoreTest.class);
-		suite.addTestSuite(SGFGameTest.class);
 		suite.addTestSuite(GnuGoEngineTest.class);
+		suite.addTestSuite(SGFGameTest.class);
 		suite.addTestSuite(TwoGTPTest.class);
 		suite.addTestSuite(GnuGoEngineTest2.class);
 		suite.addTestSuite(NoKoRuleSetTest.class);
-		suite.addTest(AllTests.suite());
+		suite.addTestSuite(CheckAllSGFFilesTest.class);
+		suite.addTestSuite(SmallBoardTest.class);
 		suite.addTestSuite(RankTest.class);
 		suite.addTestSuite(ZobristTest.class);
 		suite.addTestSuite(SGFNodeTest.class);
-		suite.addTestSuite(SmallerBoardTest.class);
 		suite.addTestSuite(SGFPropertyTest.class);
 		suite.addTestSuite(BoardTest.class);
 		suite.addTestSuite(BoardToASCIITest.class);
+		suite.addTestSuite(RandomTest.class);
 		suite.addTestSuite(SGFParserTest.class);
-		suite.addTest(GlobalTests.suite());
 		suite.addTestSuite(MoveTest.class);
-		suite.addTestSuite(SmallBoardTest.class);
-		// $JUnit-END$
+		suite.addTestSuite(SmallerBoardTest.class);
+		//$JUnit-END$
 		return suite;
 	}
 
