54007633aca62f389f9e39874160a2906dde2937
syeates <syeates@d96caf76-6f43-0410-8f8b-1b2c4ef0ccf3>
2008-03-26 04:38:20 +0000

    ensure that all tests are actually being run

--- jgogears/jgogears/GlobalTests.java
+++ jgogears/jgogears/GlobalTests.java
@@ -22,28 +22,36 @@ public class GlobalTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("all tests in project");
-		suite.addTestSuite(NodeIteratorTest.class);
-		suite.addTestSuite(TreeIteratorTest.class);
+		suite.addTestSuite(GameTest.class);
+		suite.addTestSuite(GTPParserUtilsTest.class);
+		suite.addTestSuite(GTPScoreTest.class);
+		suite.addTestSuite(GnuGoEngineTest.class);
+		suite.addTestSuite(SGFGameTest.class);
+		suite.addTestSuite(TwoGTPTest.class);
+		suite.addTestSuite(GnuGoEngineTest2.class);
+		suite.addTestSuite(NoKoRuleSetTest.class);
+		suite.addTestSuite(CheckAllSGFFilesTest.class);
+		suite.addTestSuite(SmallBoardTest.class);
+		suite.addTestSuite(RankTest.class);
+		suite.addTestSuite(ZobristTest.class);
+		suite.addTestSuite(SGFNodeTest.class);
+		suite.addTestSuite(SGFPropertyTest.class);
+		suite.addTestSuite(BoardTest.class);
+		suite.addTestSuite(BoardToASCIITest.class);
+		suite.addTestSuite(RandomTest.class);
+		suite.addTestSuite(SGFParserTest.class);
+		suite.addTestSuite(MoveTest.class);
+		suite.addTestSuite(SmallerBoardTest.class);
+
+		suite.addTestSuite(TrainerTest.class);
+		suite.addTestSuite(VertexLineariserTest.class);
 		suite.addTestSuite(NodeTest.class);
+		suite.addTestSuite(NodeIteratorTest.class);
+		suite.addTestSuite(SufogoEngineTest.class);
+		suite.addTestSuite(TreeIteratorTest.class);
 		suite.addTestSuite(ModelTest.class);
 		suite.addTestSuite(StraightVertexLineariserTest.class);
-		suite.addTestSuite(VertexLineariserTest.class);
-		suite.addTestSuite(MoveTest.class);
-		suite.addTestSuite(SGFPropertyTest.class);
-		suite.addTestSuite(SGFNodeTest.class);
-		suite.addTestSuite(SGFParserTest.class);
-		suite.addTestSuite(RankTest.class);
-		suite.addTestSuite(GTPParserUtilsTest.class);
-		suite.addTestSuite(GnuGoEngineTest.class);
-		suite.addTestSuite(BoardTest.class);
-		suite.addTestSuite(SGFGameTest.class);
-		suite.addTestSuite(GTPScoreTest.class);
-		suite.addTestSuite(GnuGoEngineTest2.class);
-		suite.addTestSuite(SmallBoardTest.class);
-		suite.addTestSuite(SmallerBoardTest.class);
-		suite.addTestSuite(NoKoRuleSetTest.class);
-		suite.addTestSuite(ZobristTest.class);
-		suite.addTestSuite(CheckAllSGFFilesTest.class);
+
 		return suite;
 	}
 
